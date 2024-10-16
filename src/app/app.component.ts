7ca2ed135aa8fc61e37c595e30c70030e9cd4077
daniel <adtdaniel87@gmail.com>
2024-10-16 16:48:56 -0600

    Creacion de componente textinput

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TextinputComponent } from './textinput/textinput.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, TextinputComponent],
+  templateUrl: './app.component.html' ,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
---
+++ src/app/textinput/textinput.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-textinput',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './textinput.component.html',
+  styleUrl: './textinput.component.css'
+})
+export class TextinputComponent {
+
+}
